1bcdc73bea6aa0b747c9dd7bd9316eba7927c715
yozafard <yozafardhs.2021@scis.smu.edu.sg>
2024-07-05 12:21:19 +0800

    test again

--- scraper_api.py
+++ scraper_api.py
@@ -43,7 +43,7 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
-options.add_argument("--remote-debugging-port=9222")
+# options.add_argument("--remote-debugging-port=9222")
 options.binary_location = "/opt/render/project/.render/chrome/opt/google/chrome/google-chrome"
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
