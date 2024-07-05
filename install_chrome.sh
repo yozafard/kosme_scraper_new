c76caa5e894f187755750c5866ec117cb4c085ff
yozafard <yozafardhs.2021@scis.smu.edu.sg>
2024-07-05 11:21:55 +0800

    updated .sh and remove .deb

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
+# Exit on error
+set -o errexit
 
-# Exit script on any error
-set -e
+# Define storage directory for Chrome
+STORAGE_DIR=/opt/render/project/.render
 
-# Update the package list and install dependencies
-# apt-get update
-# apt-get install -y wget gnupg2
+# Check if Chrome is already downloaded
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
+else
+  echo "...Using Chrome from cache"
+fi
 
-# Install Google Chrome using the downloaded setup file
-
-chmod +x ChromeSetup.exe
-./ChromeSetup.exe --install --force --accept-license
+# Add Chrome's location to the PATH
+export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
 # Install Python dependencies
 pip install -r requirements.txt
-
-# Optional: Log successful completion
-echo "Google Chrome and Python dependencies installation completed successfully."
