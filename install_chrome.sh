5a7daa8f4827c20b0369d25a1207646d3854a69a
yozafard <yozafardhs.2021@scis.smu.edu.sg>
2024-07-08 19:45:26 +0800

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -2,20 +2,26 @@
 # Exit on error
 set -o errexit
 
+if ! command -v wget &> /dev/null; then
+    echo "wget not found, installing..."
+    apt-get update
+    apt-get install -y wget
+fi
+
 # Define storage directory for Chrome
 STORAGE_DIR=/opt/render/project/.render
 
 # Check if Chrome is already downloaded
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Make sure we return to where we were
+    echo "...Downloading Chrome"
+    mkdir -p $STORAGE_DIR/chrome
+    cd $STORAGE_DIR/chrome
+    wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+    rm ./google-chrome-stable_current_amd64.deb
+    cd $HOME/project/src # Make sure we return to where we were
 else
-  echo "...Using Chrome from cache"
+    echo "...Using Chrome from cache"
 fi
 
 # Add Chrome's location to the PATH
@@ -23,3 +29,11 @@ export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
 # Install Python dependencies
 pip install -r requirements.txt
+
+# Clean up temporary files
+echo "Cleaning up..."
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+# Remove unnecessary files
+rm -rf $STORAGE_DIR/chrome/google-chrome-stable_current_amd64.deb
